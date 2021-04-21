512914790d1d38ec10ed73a3605190c6db30ee04
sylla <s67986601@gmail.com>
2021-04-21 17:22:27 +0000

    architecture: mise en place du layout.php

---
+++ public/layout.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title_of_references ?? "erreur1" ?></title>
+</head>
+
+<body>
+    <main class="main">
+
+        <header class="main__header">
+            <h3 class="main__header--title"><?php echo $title_of_site ?? "e_commerce" ?></h3>
+            <nav class="main__header--nav">
+                <p class="main__header--nav-connection"><a href="./">connexion</a></p>
+                <p class="main__header--nav-inscription"><a href="./">inscription</a></p>
+            </nav>
+        </header>
+
+
+        <section class="main__section">
+            <div class="main__section--rein">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere id corporis
+                quas quo molestiae. Ratione veniam dolor, quod nobis illo necessitatibus saepe doloribus deserunt
+                nostrum explicabo alias quibusdam tempore facilis?
+                Nesciunt temporibus fugiat ab eius repellat, quas qui, tenetur fugit amet impedit unde eos pariatur
+                exercitationem consectetur nemo ullam beatae doloribus, magnam sit laboriosam dicta ex. At laborum quod
+                non.
+                Iste modi sed, ipsum et, quaerat provident necessitatibus consequuntur at reiciendis non rerum! Dolores
+                deserunt corrupti placeat voluptate eaque nobis, molestiae quis corporis qui, labore adipisci reiciendis
+                quam quidem neque!
+                Laboriosam assumenda sed dicta ullam animi labore, accusamus, dolorem necessitatibus modi esse soluta
+                officia et odio fuga, magnam blanditiis ea enim corporis ut? Fuga ipsa veniam explicabo! Autem, itaque
+                eveniet!</div>
+        </section>
+
+        <footer class="main__footer">
+            <div class="main__footer--rein">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum a aliquid
+                doloribus maxime iure ad eum voluptatibus? Reiciendis minus, error eveniet unde maiores molestias
+                eligendi ducimus temporibus consequatur ipsum reprehenderit.</div>
+        </footer>
+    </main>
+</body>
+
+</html>
