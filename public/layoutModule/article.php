f3ca1eb1c9454e91790dde63c5401630139a9033
sylla <s67986601@gmail.com>
2021-05-13 11:25:09 +0000

    feat: article mise an place

---
+++ public/layoutModule/article.php
@@ -0,0 +1,10 @@
+<main class="article">
+    <?php for ($count = 0; $count < 10; $count++) { ?>
+        <div class="article__items">
+            <img src="../picture/article/pc<?= $count ?>.jpg" alt="" class="article__items--img">
+            <h3 class="article__items--price">19 000cfa</h3>
+            <p class="article__items--decscribe">juste un peu de phrase</p>
+        </div>
+    <?php } ?>
+  
+</main>
